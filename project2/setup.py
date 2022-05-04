948bced9e3b64142ab0abc19cb0625c69821af88
Lin Zhang <linzhank@gmail.com>
2022-05-03 20:59:49 -0500

    reformatted setup.py

--- project2/setup.py
+++ project2/setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup
-import os 
+import os
 from glob import glob
 
-package_name = 'project2'
+package_name = "project2"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
         (
             os.path.join("share", package_name, "launch"),
             glob(os.path.join("launch", "*.launch.py")),
@@ -21,16 +20,14 @@ setup(
             glob(os.path.join("urdf", "*.urdf.xacro")),
         ),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='ubuntu',
-    maintainer_email='namadoumahamane1@cub.uca.edu',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="ubuntu",
+    maintainer_email="namadoumahamane1@cub.uca.edu",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        'odom_publisher = project2.odom_publisher:main'
-        ],
+        "console_scripts": ["odom_publisher = project2.odom_publisher:main"],
     },
 )
